8fd82e6922d2c9a10c43a26021169bb5ccfbed25
Meghan <SanguineBird@users.noreply.github.com>
2015-04-01 05:07:52 -0400

    Create RepeatPrompt.h
    untested

---
+++ RepeatPrompt.h
@@ -0,0 +1,24 @@
+/*
+This is the header file for class RepeatPrompt.cpp
+*/
+
+#ifndef REPEATPROMPT_H
+#define REPEATPROMPT_H
+
+namespace RepeatMMoore{
+  class RepeatPrompt{
+    public:
+      bool repeatYN();
+        /*Precondition: function is called into a loop dependent on a boolean
+        value where a return value of 'false' will end the loop.
+        Postcondition: If the user selects 1 for 'yes', the function will
+        return true. If the user selects 0 for 'exit', the function will
+        return 'false'.If any other input is given, the prompt will repeat.*/
+
+    private:
+      bool validInput, repeat;
+      int input;
+  };
+} //end namespace TowersMMoore
+#endif //REPEATPROMPT_H
+
