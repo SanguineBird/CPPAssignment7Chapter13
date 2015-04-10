013a2fc3ec611ee245bd69b505bb5aa85abb3324
Meghan <SanguineBird@users.noreply.github.com>
2015-04-10 03:00:51 -0400

    Update TowersOfHanoi.h
    This compiles, but I'm having trouble with function moveDiscs()

--- TowersOfHanoi.h
+++ TowersOfHanoi.h
@@ -5,6 +5,9 @@ This is the header file for class TowersOfHanoi.cpp
 #ifndef TOWERSOFHANOI_H
 #define TOWERSOFHANOI_H
 
+#include <iostream> //cout
+using namespace std;
+
 namespace TowersMMoore{
   class TowersOfHanoi{
     public:
@@ -13,12 +16,15 @@ namespace TowersMMoore{
       int getMoves(){return moves;}
       int getMaxIndex(){return maxIndex;}
       char* getDiscs(){return discs;}
+      char getDiscsIndex(int index){return discs[index];}
       //recurrent function moves 1 disc:
       void moveDiscs();
       ~TowersOfHanoi();
     private:
       //prints latest movement of disc to screen using cout:
       void display(int index);
+      //returns true if all discs are on specified tower:
+      bool allDiscs(char tower);
       
       int moves;
       int maxIndex;
