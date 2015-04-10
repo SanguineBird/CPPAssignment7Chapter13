42bc11a004b8bee34ea500050108eaf01af8e485
Meghan <SanguineBird@users.noreply.github.com>
2015-04-10 03:23:50 -0400

    Update TowersOfHanoi.cpp
    Final Code

--- TowersOfHanoi.cpp
+++ TowersOfHanoi.cpp
@@ -29,38 +29,27 @@ namespace TowersMMoore{
     }
   }
 
-//Based on research
-//Source: http://www.cs.cmu.edu/~cburch/survey/recurse/hanoiimpl.html
-  void TowersOfHanoi::moveDiscs(int disc, char source, char dest, char spare){
-    if (disc == 0)
-      discs[disc] = dest;
+  //Based on research
+  //Source: http://www.cs.cmu.edu/~cburch/survey/recurse/hanoiimpl.html
+  void TowersOfHanoi::moveDisc(int disc, char source, char dest, char spare){
+    if (disc == 0){
+       discs[disc] = dest;
+       display(disc);
+       moves++;
+  	}
     else{
       moveDisc(disc - 1, source, spare, dest);
       discs[disc] = dest;
+      display(disc);
+      moves++;
       moveDisc(disc - 1, spare, dest, source);
-    } // end else
+    }
   } //end moveDiscs
 
-  //uses cout
+  //uses cout:
   void TowersOfHanoi::display(int index){
     cout << "Disc " << (index + 1) << " moved to tower " << discs[index] << endl;
   } //end display
-  
-  bool TowersOfHanoi::allDiscs(char tower){
-    bool rValue;
-    
-    for(int i = maxIndex; i >= 0; i--){
-      if (discs[i] != tower){
-        rValue = false;
-        break;
-      }
-      else
-        rValue = true;
-    }
-    
-    cout << rValue << endl;
-    return rValue;
-  } //end allDiscs
   
   TowersOfHanoi::~TowersOfHanoi()
   {delete [] discs;}
